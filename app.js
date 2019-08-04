670d726a100624955c059aa07ed225679a97f1e3
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-08-04 10:55:32 +0530

    corrected order of execution

--- app.js
+++ app.js
@@ -47,9 +47,6 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", async function (req, res) {
-
-
-
     var startDate = formatDate(new Date(Date.parse((req.body.startDate))));
     var endDate = formatDate(new Date(Date.parse((req.body.endDate))));
     var expiry = req.body.expiry;
@@ -59,71 +56,48 @@ app.post("/", async function (req, res) {
     console.log(expiry);
     console.log(priceType);
 
+    //Getting expiry dates for the trading date.
+    mysqlConnection.query("select" +
+        " distinct t1.Date , t1.Expiry , t1.`Nifty Index`, t1.`CLosest (Low)`, t1.`Closest (High)`, 'PE' as 'Option Type p' , t1.`Price puts`, 'CE' as 'Option Type c' , t2.`Price calls` " +
+        "from " +
+        "(select distinct bank_nifty.Date , bank_nifty_puts.Expiry , bank_nifty." + `${priceType}` + " as 'Nifty Index' , FLOOR(bank_nifty." + `${priceType}` + " / 100) * 100 as 'CLosest (Low)', CEIL(bank_nifty." + `${priceType}` + " / 100) * 100 as 'Closest (High)' , bank_nifty_puts." + `${priceType}` + " as 'Price puts' from bank_nifty_puts JOIN bank_nifty where bank_nifty_puts.Date = bank_nifty.Date and bank_nifty.Date between " + `'${startDate}'` + " and " + `'${endDate}'` + " and bank_nifty_puts.`Strike Price` in (FLOOR(bank_nifty." + `${priceType}` + "/100)*100)) as t1 " +
+        "left join " +
+        "(select  distinct bank_nifty.Date , bank_nifty_calls.Expiry , bank_nifty." + `${priceType}` + " as 'Nifty Index' , FLOOR(bank_nifty." + `${priceType}` + " / 100) * 100 as 'CLosest (Low)', CEIL(bank_nifty." + `${priceType}` + " / 100) * 100 as 'Closest (High)', bank_nifty_calls." + `${priceType}` + " as 'Price calls' from bank_nifty_calls JOIN bank_nifty where bank_nifty_calls.Date = bank_nifty.Date and bank_nifty.Date between " + `'${startDate}'` + " and " + `'${endDate}'` + "  and bank_nifty_calls.`Strike Price` = (CEIL(bank_nifty." + `${priceType}` + " / 100) * 100)) as t2 " +
+        "on (t1.Date = t2.Date and t1.Expiry = t2.Expiry);"
 
+        , (err, rows, fields) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(rows[0]);
 
-    function calData() {
-
-        //Getting expiry dates for the trading date.
-        mysqlConnection.query("select" +
-            " distinct t1.Date , t1.Expiry , t1.`Nifty Index`, t1.`CLosest (Low)`, t1.`Closest (High)`, 'PE' as 'Option Type p' , t1.`Price puts`, 'CE' as 'Option Type c' , t2.`Price calls` " +
-            "from " +
-            "(select distinct bank_nifty.Date , bank_nifty_puts.Expiry , bank_nifty." + `${priceType}` + " as 'Nifty Index' , FLOOR(bank_nifty." + `${priceType}` + " / 100) * 100 as 'CLosest (Low)', CEIL(bank_nifty." + `${priceType}` + " / 100) * 100 as 'Closest (High)' , bank_nifty_puts." + `${priceType}` + " as 'Price puts' from bank_nifty_puts JOIN bank_nifty where bank_nifty_puts.Date = bank_nifty.Date and bank_nifty.Date between " + `'${startDate}'` + " and " + `'${endDate}'` + " and bank_nifty_puts.`Strike Price` in (FLOOR(bank_nifty." + `${priceType}` + "/100)*100)) as t1 " +
-            "left join " +
-            "(select  distinct bank_nifty.Date , bank_nifty_calls.Expiry , bank_nifty." + `${priceType}` + " as 'Nifty Index' , FLOOR(bank_nifty." + `${priceType}` + " / 100) * 100 as 'CLosest (Low)', CEIL(bank_nifty." + `${priceType}` + " / 100) * 100 as 'Closest (High)', bank_nifty_calls." + `${priceType}` + " as 'Price calls' from bank_nifty_calls JOIN bank_nifty where bank_nifty_calls.Date = bank_nifty.Date and bank_nifty.Date between " + `'${startDate}'` + " and " + `'${endDate}'` + "  and bank_nifty_calls.`Strike Price` = (CEIL(bank_nifty." + `${priceType}` + " / 100) * 100)) as t2 " +
-            "on (t1.Date = t2.Date and t1.Expiry = t2.Expiry);"
-
-            , (err, rows, fields) => {
-                if (err) {
-                    console.log(err);
+                for (let i = 0; i < rows.length; i++) {
+                    tradingDate[i] = formatDate(new Date(JSON.parse(JSON.stringify(rows[i]['Date']))));
+                    expiryDate[i] = rows[i]['Expiry'];
+                    niftyIndex[i] = rows[i]['Nifty Index'];
+                    closesLow[i] = rows[i]['CLosest (Low)'];
+                    closesHigh[i] = rows[i]['Closest (High)'];
+                    pricePut[i] = rows[i]['Price puts'];
+                    priceCall[i] = rows[i]['Price calls'];
+                    sum[i] = parseFloat(pricePut[i]) + parseFloat(priceCall[i]);
                 }
 
-                else {
-
-                    console.log(JSON.parse(JSON.stringify(rows[0])));
-
-                    for (let i = 0; i < JSON.parse(JSON.stringify(rows)).length; i++) {
-                        tradingDate[i] = formatDate(new Date(JSON.parse(JSON.stringify(rows[i]['Date']))));
-                        expiryDate[i] = JSON.parse(JSON.stringify(rows[i]['Expiry']));
-                        niftyIndex[i] = JSON.parse(JSON.stringify(rows[i]['Nifty Index']));
-                        closesLow[i] = JSON.parse(JSON.stringify(rows[i]['CLosest (Low)']));
-                        closesHigh[i] = JSON.parse(JSON.stringify(rows[i]['Closest (High)']));
-                        pricePut[i] = JSON.parse(JSON.stringify(rows[i]['Price puts']));
-                        priceCall[i] = JSON.parse(JSON.stringify(rows[i]['Price calls']));
-                        sum[i] = parseFloat(pricePut[i]) + parseFloat(priceCall[i]);
-                    }
-
-                }
-
-            })
-    }
-    calData();
-    function loadTable() {
-
-        res.render("table", {
-            tradingDate: tradingDate,
-            expiryDate: expiryDate,
-            priceType: priceType,
-            niftyIndex: niftyIndex,
-            closesLow: closesLow,
-            closesHigh: closesHigh,
-            pricePut: pricePut,
-            priceCall: priceCall,
-            sum: sum
-
+                res.render("table", {
+                    tradingDate: tradingDate,
+                    expiryDate: expiryDate,
+                    priceType: priceType,
+                    niftyIndex: niftyIndex,
+                    closesLow: closesLow,
+                    closesHigh: closesHigh,
+                    pricePut: pricePut,
+                    priceCall: priceCall,
+                    sum: sum
+                });
+            }
         });
-
-    }
-    loadTable();
-
-
 });
 
 app.listen(3000, function () {
     console.log("App started at port 3000");
 });
-
-const Waiter = (t) => new Promise(function (resolve, reject) {
-    setTimeout(function () {
-        resolve()
-    }, t)
-})
